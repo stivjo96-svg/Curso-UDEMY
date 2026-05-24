a7d1911f5416b6c0f4c4ab50b2723231c76dc409
Stiven Vallejo <stivjo96@gmail.com>
2026-05-23 21:13:01 -0500

    ejercicio 1

---
+++ primeros pasos/variables.js
@@ -0,0 +1,16 @@
+var saludo = 'hola mundo'
+if(saludo){
+    var saludo = 'Hola'
+    console.log(saludo)
+}
+console.log(saludo)
+
+let nombre = 'Joseph'
+if(nombre){
+    let nombre = 'Stiven'
+    console.log(nombre)
+}
+console.log(nombre)
+
+const PI = 3.14
+console.log(PI)
