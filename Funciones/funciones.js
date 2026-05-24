29d69b57c94ff8fa6e61b0025eab1bcb1e7a876b
Stiven Vallejo <stivjo96@gmail.com>
2026-05-23 23:07:46 -0500

    ejercicio 5

---
+++ Funciones/funciones.js
@@ -0,0 +1,16 @@
+function saludo(){
+    console.log('Hola a todos')
+}
+
+var miSuma = function (a,b,c){
+    return a + b + c
+}
+
+saludo()
+console.log(miSuma(3,6,9))
+console.log(miSuma(4,12,6))
+
+var a2 = 4
+var b2 = 9
+var c2 = 2
+console.log(miSuma(a2,b2,c2))
