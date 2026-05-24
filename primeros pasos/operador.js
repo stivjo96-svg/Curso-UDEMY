ac3a80c7a6867691b2a400f0e520c4d36cf4bc67
Stiven Vallejo <stivjo96@gmail.com>
2026-05-23 22:25:02 -0500

    ejercicio 4

---
+++ primeros pasos/operador.js
@@ -0,0 +1,21 @@
+var a = 2 
+a = a + 3
+console.log(a)
+
+var a2 = 5
+console.log(a == a2)
+console.log(a >= a2)
+
+var b = 7
+b++
+console.log(b)
+
+var c = 5
+c--
+console.log(c)
+
+var v = true 
+var f = false 
+console.log(v && f)
+console.log(v || f)
+console.log(!v)
