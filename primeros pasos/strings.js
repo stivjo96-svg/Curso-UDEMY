4939f64c25289c582118c831ce6774cb9a078fba
Stiven Vallejo <stivjo96@gmail.com>
2026-05-23 21:42:06 -0500

    ejercicio 2

---
+++ primeros pasos/strings.js
@@ -0,0 +1,9 @@
+var nombre = 'Stiven'
+var edad = 29
+console.log('Mi nombre es '+nombre+' y tengo '+edad+' años')
+
+var nombreMayus = nombre.toUpperCase()
+
+console.log(nombreMayus)
+console.log(nombre.toLowerCase())
+console.log(nombre.length)
---
+++ primeros pasos/tiposv.js
@@ -0,0 +1,19 @@
+var nombre = 'Stiven'
+console.log(nombre)
+var edad = 29
+console.log(edad)
+const PI = 3.14
+console.log(PI)
+var verdadero = true
+var falso = false
+console.log(verdadero)
+console.log(falso)
+var nulo = null
+console.log(nulo)
+var indefinido = undefined
+console.log(indefinido)
+
+console.log(typeof 'Stiven')
+
+const logN = Math.LN2
+console.log(logN)
