bfeb1c3768c84c151e875d7e83c2e9f5ad486e00
Stiven Vallejo <stivjo96@gmail.com>
2026-05-25 21:40:56 -0500

    ejercicio 11

---
+++ Condicionales/if-else.js
@@ -0,0 +1,21 @@
+var edad = 7
+
+if(edad >= 18 && edad <= 30){
+    console.log("Joven adulto")
+}else if(edad < 18){
+    console.log("Niño o adolescente")
+}else if(edad > 30 && edad <= 55){
+    console.log("Adulto")
+}else if(edad > 55){
+    console.log("Anciano")
+}else{
+    console.log("Indefinido")
+}
+
+/*var variable = true
+
+if(variable){
+    console.log("Verdadero")
+}else{
+    console.log("Falso")
+}*/
