f56a249cba22f6412542ec8d4f22c4e9c99b6b05
Stiven Vallejo <stivjo96@gmail.com>
2026-05-29 23:38:24 -0500

    Actualizar servidor Express con rutas GET POST PATCH DELETE

--- Nuevo-js/serverNode/server.js
+++ Nuevo-js/serverNode/server.js
@@ -1,5 +1,9 @@
 var express = require("express")
 var app = express()
-app.use('/',(req,res) => res.send('Hola Stiven'))
 
-app.listen(3000,()=>console.log("Escuchando en el puerto 3000"))
+app.get('/home', (req, res) => res.send('Hola Stiven'))
+app.post('/login', (req, res) => res.send('Bienvenido Usuario'))
+app.patch('/actualizar', (req, res) => res.send('Usuario actualizado'))
+app.delete('/borrar', (req, res) => res.send('Usuario eliminado'))
+
+app.listen(3000, () => console.log("Escuchando en el puerto 3000"))
